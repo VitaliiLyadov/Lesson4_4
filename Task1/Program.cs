200031854e8f4db432e8ccdfe59f7b67887051aa
Vitalii <LVitek2506@yandex.ru>
2023-04-04 11:15:49 +0500

    Task2

---
+++ Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿//  Задача 1: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+int ReadInt(string message)
+{
+    Console.Write($"{message} > ");
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int SumDigits(int number)
+{
+    int sum = 0;
+    for (int i = 1; i <= number; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
+
+int value = ReadInt("Введите число");
+
+int sum = SumDigits(value);
+System.Console.WriteLine($"Сумма числа {value} равна {sum}");
---
+++ Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+int ReadInt(string message)
+{
+    Console.Write($"{message} > ");
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int SumDigits(int number)
+{
+    int sum = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        sum = sum * i;
+    }
+    return sum;
+}
+
+int value = ReadInt("Введите число");
+
+int sum = SumDigits(value);
+System.Console.WriteLine($"Произведение числа {value} равна {sum}");
